e85d39ec7cd80eb336b5e3bbe37306e134033422
Christian <pngue006@uottawa.ca>
2023-12-08 15:17:21 -0500

    Setup auth

--- src/App.js
+++ src/App.js
@@ -1,25 +1,72 @@
-import logo from './logo.svg';
 import './App.css';
+import firebase from './firebase';
+import 'firebase/firestore';
+import 'firebase/auth';
+
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { useCollectionData } from 'react-firebase-hooks/firestore';
+import { useHistory } from 'react-router-dom';
+
+firebase.initializeApp({ 
+  apiKey: "AIzaSyA1qaLQXpTyabgrvBSEytzESHkfe4hT_cc",
+  authDomain: "partfolio-project.firebaseapp.com",
+  projectId: "partfolio-project",
+  storageBucket: "partfolio-project.appspot.com",
+  messagingSenderId: "1084826388216",
+  appId: "1:1084826388216:web:fdd9a3136e7c7b7d7add0d",
+  measurementId: "G-9GC5XVZTPZ"
+})
+
+const auth = firebase.auth();
+const firestore = firebase.firestore();
 
 function App() {
+
+  const [user] = useAuthState(auth);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        
       </header>
+      {/* If user not logged in, show sign in prompt; otherwise show main page. */}
+      <section>
+        {user ? <Main /> : <SignIn />}
+      </section>
     </div>
   );
 }
 
+function SignIn() {
+  // Sign in with Google
+  const signInWithGoogle = () => {
+    const provider = new firebase.auth.GoogleAuthProvider();
+    auth.signInWithPopup(provider);
+  }
+
+  return (
+    <div>
+      <button onClick={signInWithGoogle}>Sign in with Google</button>
+    </div>
+  )
+}
+
+function SignOut() {
+  // Sign out
+  return auth.currentUser && (
+    <button onClick={() => auth.signOut()}>Sign Out</button>
+  )
+}
+
+function Main() {
+  const history = useHistory();
+
+  const goToMainPage = () => {
+    history.push('/main');
+  };
+
+  return (
+    goToMainPage()
+  );
+}
 export default App;
